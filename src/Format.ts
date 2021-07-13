e911288ac9e2c5d8b809cdad8caa183e2c80b0aa
Thomas Volk (MacBook) <info@thomasvolk.de>
2021-07-13 21:34:10 +0200

    add json decoding

--- src/Format.ts
+++ src/Format.ts
@@ -9,11 +9,32 @@ export interface Format {
 }
 
 export class JsonFormat implements Format {
-    decode(source: string): Configuration {
-        return new Configuration(new Universe(0, 0), new EEFFRule(0,0,0,0));
+    private jsonToUniverse(obj: any): Universe {
+        const u = new Universe(obj.width, obj.height);
+        u.reset();
+        (obj.cells as Array<any>).forEach((c) => {
+            const cell = u.getCell(c.x, c.y)
+            cell.enterValue(c.v)
+            cell.apply(true)
+        })
+        return u
     }
 
-    private ruleToJson(rule: Rule): Object {
+    private jsonToRule(obj: any): Rule {
+        return new EEFFRule(
+            obj.el,
+            obj.eu,
+            obj.fl,
+            obj.fu
+        )
+    }
+
+    decode(source: string): Configuration {
+        const root = JSON.parse(source)
+        return new Configuration(this.jsonToUniverse(root.universe), this.jsonToRule(root.rule));
+    }
+
+    private ruleToJson(rule: Rule): any {
         if(rule instanceof EEFFRule) {
             const r = rule as EEFFRule
             return {"type":"EEFF", "el":r.el, "eu":r.eu, "fl":r.fl, "fu":r.fu}
@@ -21,14 +42,14 @@ export class JsonFormat implements Format {
         return {};
     }
 
-    private cellsToJson(cells: Array<Cell>): Array<Object> {
-        return cells.map(c => {
+    private cellsToJson(cells: Array<Cell>): Array<any> {
+        return cells.filter((c) => c.getValue() > 0).map(c => {
             const o = {"x": c.getX(), "y": c.getY(), "v": c.getValue()}
             return o
         })
     }
 
-    private universeToJson(universe: Universe): Object {
+    private universeToJson(universe: Universe): any {
         const cells = this.cellsToJson(universe.getCells())
         return {
             "type":"2D", 
--- test/Format.spec.ts
+++ test/Format.spec.ts
@@ -17,7 +17,16 @@ describe('JsonFormat', () => {
         config.universe.getCell(1, 1).enterValue(1).apply()
         const f = new JsonFormat()
         expect(f.encode(config)).to.equal('{"universe":{"type":"2D","endless":true,"width":2,"height":2,' +
-        '"cells":[{"x":0,"y":0,"v":1},{"x":1,"y":0,"v":0},{"x":0,"y":1,"v":0},{"x":1,"y":1,"v":1}]},' +
+        '"cells":[{"x":0,"y":0,"v":1},{"x":1,"y":1,"v":1}]},' +
         '"rule":{"type":"EEFF","el":2,"eu":3,"fl":3,"fu":3}}')
+    });
+    it('should decode the configuration from a json string', () => {
+        const json = '{"universe":{"type":"2D","endless":true,"width":4,"height":2,' +
+        '"cells":[{"x":0,"y":0,"v":1},{"x":1,"y":0,"v":0},{"x":0,"y":1,"v":0},{"x":1,"y":1,"v":1}]},' +
+        '"rule":{"type":"EEFF","el":2,"eu":3,"fl":3,"fu":3}}'
+        const f = new JsonFormat()
+        const cfg = f.decode(json)
+        expect(cfg.universe.height).to.equal(2)
+        expect(cfg.universe.width).to.equal(4)
     });
 });
