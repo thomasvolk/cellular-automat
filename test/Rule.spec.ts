3c4a9c0df9df1350498cca57363845c430126545
Thomas Volk (MacBook) <info@thomasvolk.de>
2021-07-25 22:11:03 +0200

    add tests for BSRule

--- test/Rule.spec.ts
+++ test/Rule.spec.ts
@@ -1,54 +1,66 @@
 import { expect } from 'chai';
 import {
+    normalizeToOneOrZero,
+    BSRule,
     EEFFRule
-} from "../src/CellularAutomat";
-import {
-    normalizeToOneOrZero
 } from "../src/Rule";
 
-describe('ConwayRule', () => {
+describe('utils', () => {
     it('should normalizeToOneOrZero', () => {
         expect(
             normalizeToOneOrZero([5, 0.7, 0, 1, -1, 0]).join(",")
         ).to.equal("1,1,0,1,0,0")
     });
-    it('should implement the rules correctly', () => {
-        const testData = [
-            {
-                'cell': 1,
-                'neighbours': [1, 1, 0, 0, 0, 0],
-                'expected': 1
-            },
-            {
-                'cell': 1,
-                'neighbours': [1, 1, 0, 0, 0, 0, 0, 0, 0],
-                'expected': 1
-            },
-            {
-                'cell': 0,
-                'neighbours': [1, 1, 0, 0, 0, 0, 0, 0, 0],
-                'expected': 0
-            },
-            {
-                'cell': 0,
-                'neighbours': [1, 1, 1, 0, 0, 0, 0, 0, 0],
-                'expected': 1
-            },
-            {
-                'cell': 1,
-                'neighbours': [1, 1, 1, 1, 0, 0, 0, 0, 0],
-                'expected': 0
-            },
-            {
-                'cell': 1,
-                'neighbours': [1, 0, 0, 0, 0, 0, 0, 0, 0],
-                'expected': 0
-            }
-        ]
-        const conwayRule = new EEFFRule(2, 3, 3, 3)
+})
+
+describe('ConwayRule', () => {
+    const testData = [
+        {
+            'cell': 1,
+            'neighbours': [1, 1, 0, 0, 0, 0],
+            'expected': 1
+        },
+        {
+            'cell': 1,
+            'neighbours': [1, 1, 0, 0, 0, 0, 0, 0, 0],
+            'expected': 1
+        },
+        {
+            'cell': 0,
+            'neighbours': [1, 1, 0, 0, 0, 0, 0, 0, 0],
+            'expected': 0
+        },
+        {
+            'cell': 0,
+            'neighbours': [1, 1, 1, 0, 0, 0, 0, 0, 0],
+            'expected': 1
+        },
+        {
+            'cell': 1,
+            'neighbours': [1, 1, 1, 1, 0, 0, 0, 0, 0],
+            'expected': 0
+        },
+        {
+            'cell': 1,
+            'neighbours': [1, 0, 0, 0, 0, 0, 0, 0, 0],
+            'expected': 0
+        }
+    ]
+
+    it('should implement the EEFFRule correctly', () => {
+        const conwayEEFFRule = new EEFFRule(2, 3, 3, 3)
         testData.forEach(testSet => {
             expect(
-                conwayRule.calculateNewValue(testSet.cell, testSet.neighbours)
+                conwayEEFFRule.calculateNewValue(testSet.cell, testSet.neighbours)
+            ).to.equal(testSet.expected);
+        });
+    });
+
+    it('should implement the BSRule correctly', () => {
+        const conwayBSRule = new BSRule([3], [2, 3])
+        testData.forEach(testSet => {
+            expect(
+                conwayBSRule.calculateNewValue(testSet.cell, testSet.neighbours)
             ).to.equal(testSet.expected);
         });
     });
